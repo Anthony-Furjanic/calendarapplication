192bd8619a9ceccbaa7992b82de5a17510140e81
unknown <anthonyf427@hotmail.com>
2022-10-28 20:38:23 -0400

    Started JS functions and added CSS

---
+++ Javascript.js
@@ -0,0 +1,16 @@
+// alert("hello")
+const v= new Date()
+let hour= v.getHours()
+hour= 1
+
+// Create lines for each case and the color code for when calendar is
+// not available for time period
+case 9: document.getElementById("9").style.backgroundColor="red"
+case 10: document.getElementById("10").style.backgroundColor="red"
+case 11: document.getElementById("11").style.backgroundColor="red"
+case 12: document.getElementById("11").style.backgroundColor="red"
+case 13: document.getElementById("11").style.backgroundColor="red"
+case 14: document.getElementById("11").style.backgroundColor="red"
+case 15: document.getElementById("11").style.backgroundColor="red"
+case 16: document.getElementById("11").style.backgroundColor="red"
+case 17: document.getElementById("11").style.backgroundColor="red"
